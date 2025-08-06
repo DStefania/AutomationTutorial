2bff9f6fc2754573acd9c6ef208b11a464e281ad
DStefania <david_stefania89@yahoo.com>
2025-08-06 19:26:27 +0300

    Am adaugat un nou pipeline

---
+++ src/test/java/suite/Suite.java
@@ -0,0 +1,10 @@
+package suite;
+
+public class Suite {
+    public static final String REGRESSION_SUITE ="@Regression";
+    public static final String SANITY_SUITE="@Sanity";
+    public static final String ALERTWINDOWFRAME_SUITE="@AlertWindowFrame";
+    public static final String FORM_SUITE="@Forms";
+    public static final String ELEMENTS_SUITE="@Elements";
+
+}
--- src/test/java/tests/AlertsTest.java
+++ src/test/java/tests/AlertsTest.java
@@ -6,10 +6,11 @@ import pages.AlertFrameWindowPage;
 import pages.AlertPage;
 import pages.IndexPage;
 import sharedData.SharedData;
+import suite.Suite;
 
 public class AlertsTest extends SharedData {
 
-    @Test
+    @Test( groups = {Suite.REGRESSION_SUITE, Suite.SANITY_SUITE, Suite.ALERTWINDOWFRAME_SUITE})
     public void testMethod() {
         IndexPage indexPage = new IndexPage(getDriver());
         indexPage.interactWithAlertsFrameWindowMenu();
